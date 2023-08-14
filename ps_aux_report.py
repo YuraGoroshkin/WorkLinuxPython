25f820efcbf0325ecd77d1c86bb07b991541d4d9
ubuntu <gyrra777@gmail.com>
2023-08-15 00:22:13 +0300

    50%

--- ps_aux_report.py
+++ ps_aux_report.py
@@ -5,7 +5,7 @@ def create_data():
     out = start_ps_aux().stdout
     all_list = str(out).split(sep='\\n')
     result_count_process = len(all_list) - 1
-    user = []
+    users = []
     count_user = {}
     cpu = float(0)
     mem = float(0)
@@ -16,19 +16,27 @@ def create_data():
         list_this_obj = object_list.split()
         cpu += float(list_this_obj[2])
         mem += float(list_this_obj[4])
-        if list_this_obj[0] not in user:
-            user.append(list_this_obj[0])
-        elif list_this_obj[0] in user:
-            i = user.index(list_this_obj[0])
+        if list_this_obj[0] not in users:
+            users.append(list_this_obj[0])
+        elif list_this_obj[0] in users:
+            i = users.index(list_this_obj[0])
             key = str(i)
             if key not in count_user:
-                count_user.update({str(i): 0})
+                count_user.update({str(i): [0, i]})
             if key in count_user:
-                count_user[str(i)] = count_user.get(str(i)) + 1
+                count_user[str(i)][0] = count_user.get(str(i))[0] + 1
     mem = float("{0:.1f}".format(mem / 1000000))
     cpu = round(cpu)
-    data = (f'Отчёт о состоянии системы:\n  Пользователи системы:{user}\n  Процессов запущено:{result_count_process}\n  '
-            f'Пользовательских процессов:{user} {count_user}\n  Всего памяти используется:{mem} mb\n  Всего CPU используется:{cpu} %')
+    string_for_user_process = ''
+    for index, user in enumerate(users):
+        dict_key = str(index)
+        if dict_key in count_user:
+            user_processes = count_user[dict_key][0] + 1
+            string_for_user_process += f"  {user}: {user_processes}\n"
+        else:
+            string_for_user_process += f"  {user}: 1\n"
+    data = (f'Отчёт о состоянии системы:\n  Пользователи системы:{users}\n  Процессов запущено:{result_count_process}\n  '
+            f'Пользовательских процессов:\n{string_for_user_process}  Всего памяти используется:{mem} mb\n  Всего CPU используется:{cpu} %')
     return data
 
 
